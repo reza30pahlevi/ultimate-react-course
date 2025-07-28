67449cb24ed7f75ce3e2178a69c0c89b16304b44
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-07-28 21:47:56 +0700

    destructuring-1-starter

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -142,3 +142,19 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+// cara normal
+// const book = getBook(2);
+// const title = book.title;
+// const author = book.author;
+// console.log(title, author);
+
+// cara destructuring
+const book = getBook(2);
+book;
+
+const { title, author, genres } = book;
+console.log(title, author, genres);
+
+const [primaryGenre, secondaryGenre] = genres;
+console.log(primaryGenre, secondaryGenre);
